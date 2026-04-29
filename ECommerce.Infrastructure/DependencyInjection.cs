b1f03810d4687b5bff02ddb9f06254059ccb7e0e
Muhammad Asaad <meshmuhammadasaad@gmail.com>
2026-04-30 01:51:22 +0300

    add asnoTraching on scope of project

--- ECommerce.Infrastructure/DependencyInjection.cs
+++ ECommerce.Infrastructure/DependencyInjection.cs
@@ -10,7 +10,10 @@ public static class DependencyInjection
         var connectionString = configuration.GetConnectionString("DefaultConnection") ??
             throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString)
+        .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+        );
 
         services.AddIdentityCore<ApplicationUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
